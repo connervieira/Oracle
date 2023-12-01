7fffb2aaa6e3a967ebdbbe656e0f5acb722352bd
Conner Vieira <cvieira@v0lttech.com>
2023-11-30 22:04:28 -0500

    Added basic authentication

--- receiver/receiver.php
+++ receiver/receiver.php
@@ -1,7 +1,14 @@
 <?php
+include "./config.php"
+
 $received_data = strval($_POST["image"]); // Get the submitted image data.
 $processed_data = json_decode($received_data, true); // Decode the JSON data received.
 
-$image_data = base64_decode($processed_data["image"]); // Decode the image from the received data.
-file_put_contents("./test.jpg", $image_data);
+if (in_array($processed_data["identifier"], config["auth"]["permitted_devices"])) {
+    $image_data = base64_decode($processed_data["image"]); // Decode the image from the received data.
+    file_put_contents("./test.jpg", $image_data);
+    echo "Success";
+} else {
+    echo "Permission denied";
+}
 ?>

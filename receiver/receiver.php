e16932c87e7986a45586cca628ec55ef168b92c4
Conner Vieira <cvieira@v0lttech.com>
2023-11-30 22:06:09 -0500

    Update receiver.php
    Fixed syntax error

--- receiver/receiver.php
+++ receiver/receiver.php
@@ -4,7 +4,7 @@ include "./config.php"
 $received_data = strval($_POST["image"]); // Get the submitted image data.
 $processed_data = json_decode($received_data, true); // Decode the JSON data received.
 
-if (in_array($processed_data["identifier"], config["auth"]["permitted_devices"])) {
+if (in_array($processed_data["identifier"], $config["auth"]["permitted_devices"])) {
     $image_data = base64_decode($processed_data["image"]); // Decode the image from the received data.
     file_put_contents("./test.jpg", $image_data);
     echo "Success";

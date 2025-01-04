3eb1aec595a0063e3f1d8794b060ac0d7b59f5b4
Conner Vieira <cvieira@v0lttech.com>
2025-01-04 18:34:35 -0500

    Switched to Base64 image display

--- receiver/index.php
+++ receiver/index.php
@@ -1,13 +1,14 @@
 <?php
+$image_data = base64_encode(file_get_contents("/dev/shm/oracle.jpg"));
 
 ?>
 <!DOCTYPE html>
 <html>
     <head>
         <title>Oracle</title>
-        <meta http-equiv="refresh" content="1" />
+        <meta http-equiv="refresh" content="10" />
     </head>
     <body>
-        <img style="width:100%;" src="./test.jpg">
+        <img style="width:100%;" src="data:image/jpg;base64,<?php echo $image_data; ?>">
     </body>
 </html>
--- receiver/receiver.php
+++ receiver/receiver.php
@@ -2,6 +2,13 @@
 $received_data = strval($_POST["image"]); // Get the submitted image data.
 $processed_data = json_decode($received_data, true); // Decode the JSON data received.
 
-$image_data = base64_decode($processed_data["image"]); // Decode the image from the received data.
-file_put_contents("./test.jpg", $image_data);
+//print_r($processed_data);
+
+if ($processed_data["identifier"] == "g7!#73gb") {
+    $image_data = base64_decode($processed_data["image"]); // Decode the image from the received data.
+    file_put_contents("/dev/shm/oracle.jpg", $image_data);
+    echo "Success";
+} else {
+    echo "Permission denied";
+}
 ?>
